9d86b4fc1f7d160bd8bdcde6bb5bf39ceaa85a68
Arlen Espino <trenod159@hotmail.com>
2015-03-24 12:07:03 -0600

    prueba
    probando el github

---
+++ appCorreo/application/views/vista.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	 <link href= <?php echo base_url() ?>"/assets/css/bootstrap.min.css" rel="stylesheet">
+	<title>Main</title>
+</head>
+<body>
+	<h1><center>Hello world</center></h1>
+
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src= <?php echo base_url() ?>"/assets/js/bootstrap.min.js"></script>
+
+</body>
+</html>
