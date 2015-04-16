978f38cf9e7356306af5007d16d3dd1a0c2bb31d
Arlen Espino <trenod159@hotmail.com>
2015-04-16 14:57:17 -0600

    prueba de cambios
    revisando si los cambios estan bien

--- appCorreo/envioCorreos.php
+++ appCorreo/envioCorreos.php
@@ -3,15 +3,9 @@
 include("class.phpmailer.php");
 include("class.smtp.php"); 
 
-<<<<<<< HEAD
 			$servername= '127.0.0.1';
 			$user = 'root';
 			$password = '';
-=======
-		$servername= '127.0.0.1';
-		$user = 'root';
-		$password = '';
->>>>>>> origin/master
 
 		try {
     		$conn = new PDO("mysql:host=$servername;dbname=correos", $user,$password);
@@ -30,7 +24,6 @@ include("class.smtp.php");
     		{
     			echo "Connection failed: " . $e->getMessage();
     		}
-<<<<<<< HEAD
     function getUsers($servername,$user,$password){
     	try {
     		$conn = new PDO("mysql:host=$servername;dbname=correos", $user,$password);
@@ -80,8 +73,7 @@ include("class.smtp.php");
 			$mail->Username = "santiesmar@gmail.com"; 
 			$mail->Password = "dorff147/"; 
 			$mail->Port = 465; 
-			echo $name ;
-    		echo $email ;
+			
     		
 		
 
@@ -103,59 +95,6 @@ include("class.smtp.php");
 			$servername= '127.0.0.1';
 			$user = 'root';
 			$password = '';
-=======
-    		
-    		
-    		
-    		for ($i=0; $i<count($registro) ; $i++) {
-    					
-    			$servername= '127.0.0.1';
-				$user = 'root';
-				$password = '';
-    			$conn = new PDO("mysql:host=$servername;dbname=correos", $user,$password);
-   
-    			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    			echo "Connected successfully" . "\n"; 
-				$sql = sprintf("SELECT `name`,`email` FROM `users` WHERE  `id`=".$registro[$i]["iduser"] );
-				
-				$consulta = $conn->prepare($sql);
-				$consulta->execute();
-				$user = $consulta->fetchAll();
-						
-    			
-			
-		$mail = new PHPMailer();
-
-//Luego tenemos que iniciar la validación por SMTP:
-		$mail->IsSMTP();
-		$mail->SMTPAuth = true;
-		$mail->SMTPSecure = "ssl"; 
-		$mail->Host = "smtp.gmail.com"; // SMTP a utilizar. Por ej. smtp.elserver.com
-		$mail->Username = "santiesmar@gmail.com"; 
-		$mail->Password = "dorff147/"; 
-		$mail->Port = 465; 
-
-		
-
-		$mail->From = $user[$i]['email']; 
-		$mail->FromName = $user[$i]['name'];
-		$mail->Subject = $registro[$i]['asunto'];
-		$mail->AltBody = "Este es un mensaje";  
-		$mail->MsgHTML($registro[$i]['mensaje']); 
-		
-		$mail->AddAddress($registro[$i]['destinatario']); 
-		$mail->IsHTML(true); 
-		
-		
-		$exito = $mail->Send(); // Envía el correo.
-
-
-		if($exito){
-			try {
-					$servername= '127.0.0.1';
-				$user = 'root';
-				$password = '';
->>>>>>> origin/master
     		$conn = new PDO("mysql:host=$servername;dbname=correos", $user,$password);
     // set the PDO error mode to exception
     		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -172,7 +111,6 @@ include("class.smtp.php");
     		echo $stmt->rowCount() . " Record updated successfully" ."\n" ;
 
     
-<<<<<<< HEAD
     	}
 		catch(PDOException $e)
     	{
@@ -190,21 +128,6 @@ include("class.smtp.php");
 
 
 }	
-=======
-    		}
-			catch(PDOException $e)
-    		{
-    		echo "Connection failed: " . $e->getMessage();
-    		}
-			$conn = null;
-			
-			}else{
-			echo "Hubo un inconveniente. Contacta a un administrador";
-			}
-
-
-		}	
->>>>>>> origin/master
 	
 
 
